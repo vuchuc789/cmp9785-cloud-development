8adf18f1591553247f2b8614fa50eb52a65d495c
Vu Chuc <vuchuc781999@gmail.com>
2025-04-12 10:24:03 +0100

    Install Observability Components

--- infra/terraform/modules/kubernetes/namespaces.tf
+++ infra/terraform/modules/kubernetes/namespaces.tf
@@ -2,10 +2,16 @@ resource "kubernetes_namespace" "database" {
   metadata {
     name = "database"
   }
+
+  depends_on = [helm_release.argocd]
 }
 
 resource "kubernetes_namespace" "web_api" {
   metadata {
     name = "web-api"
   }
+
+  depends_on = [helm_release.argocd]
 }
+
+# Add newly created namespaces to depends_on of argocd_apps
